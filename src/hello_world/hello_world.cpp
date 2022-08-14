6a80f4917aaf01d6aa2ee618032658b507afd91f
Arnautu Stefan <arnautu.stefan93@gmail.com>
2022-08-14 20:16:17 +0000

    Add gtest and test

--- src/hello_world/hello_world.cpp
+++ src/hello_world/hello_world.cpp
@@ -1,14 +1,7 @@
 #include "hello_world.hpp"
 
-#include <iostream>
-
 namespace CMakeTest::HelloWorld {
 
 std::string fn_hello_world() { return "hello world\n"; }
 
 }  // namespace CMakeTest::HelloWorld
-
-int main() {
-  std::cout << CMakeTest::HelloWorld::fn_hello_world();
-  return 0;
-}
